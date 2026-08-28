653a440c02d119870d93dc7fcb9ae47c540eb1e6
Emmanuel Martínez <emmanuelmartinez@ravn.co>
2026-08-28 16:22:42 -0500

    feat: wire up modules, global filters, Swagger, and validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RolesModule } from './roles/roles.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [ScheduleModule.forRoot(), RolesModule, PrismaModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
